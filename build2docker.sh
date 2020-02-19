88e7abab18c7385772d406dd286d9c88c9c2f0ac
shao zheng bo <shaoiszhengbo@163.com>
2020-02-19 09:47:57 +0800

    update 修改删除image命令

--- build2docker.sh
+++ build2docker.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-docker image rm assistant_springboot
+docker image rm assistant_springboot/assistant
 
 mvn package docker:build
 
-docker run -p 8080:8080 -t assistant_springboot
+docker run -d -p 8080:8080 -t assistant_springboot/assistant
