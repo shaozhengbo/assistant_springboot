d985fd8cc7f844c3ccbe65980f0c4d710546bccc
shao zheng bo <shaoiszhengbo@163.com>
2020-02-21 08:48:26 +0800

    update 修改docker发布端口

--- build2docker.sh
+++ build2docker.sh
@@ -8,4 +8,4 @@ docker image rm assistant_springboot/assistant
 
 mvn package docker:build
 
-docker run -d -p 8080:8080 --name assistant_springboot -t assistant_springboot/assistant
+docker run -d -p 443:443 -p 8080:8080 --name assistant_springboot -t assistant_springboot/assistant
